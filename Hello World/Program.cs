456b2da28ae4d62cf6d5860a3b110f7024728218
Kurs <Kurs@PC01>
2016-09-14 16:54:03 +0200

    Add project files.

---
+++ Hello World/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Antwort;
+            do
+            {
+                Console.WriteLine("Hallo bist du da?");
+                Antwort = Console.ReadLine();
+                if (Antwort == "Ja")
+                    Console.WriteLine("Ich bin auch da!");
+                if (Antwort == "Nein")
+                    Console.WriteLine("Wenn du nicht da bist gehe ich jetzt!");
+
+                if (Antwort == "nein")
+                {
+                    Console.WriteLine("Wenn du nicht da bist gehe ich jetzt!");
+                    Antwort = "Nein";
+                    break;
+                }
+            
+
+
+            }
+
+            while (Antwort != "Nein");
+
+            Console.WriteLine("Tschüß!");
+            Console.ReadKey();
+        }
+    }
+}
+ //while (Antwort != "Ja" || Antwort != "Nein");
